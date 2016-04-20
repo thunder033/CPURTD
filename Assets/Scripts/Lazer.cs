163391bf48c6daaf78ea7c9b7881c7acd52bb58e
Thomas Landi (RIT Student) <txl3829@ad.rit.edu>
2016-04-20 14:52:34 -0400

    Started level and some tower scripting

---
+++ Assets/Scripts/Lazer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Tower))]
+
+public class Lazer : MonoBehaviour, TowerPower {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Attack ()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface TowerPower
+{
+    void Attack();
+}
+
+public class Tower : MonoBehaviour {
+
+    public GameManager manager;
+    public int range;
+    public TowerPower attack;
+
+	// Use this for initialization
+	void Start () {
+        range = int.MaxValue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    foreach (GameObject drone in manager.minions)
+        {
+            if (Vector3.Distance(drone.transform.position, gameObject.transform.position) <= range)
+            {
+                attack.Attack();
+            }
+        }
+	}
+}
