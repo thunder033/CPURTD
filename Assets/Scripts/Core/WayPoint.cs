452586f986ab44f4d4340f997df1095e6086ae68
Greg Rozmarynowycz <crashtest1@live.com>
2016-05-20 17:47:28 -0400

    import map base, hide way points in game, adjust positioning of stuff

---
+++ Assets/Scripts/Core/WayPoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class WayPoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<MeshRenderer>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
